b6215f5c201e92b8c4f18782472d76571e88873e
Janusz Czapski <januszczapski@gmail.com>
2020-04-25 19:00:33 +0100

    Updated colour scheme, added icon for lists, minor styling adjustments Adjusted padding and created new button hover animation Added PurgeCSS & minification to build process, removed unused packages

--- postcss.config.js
+++ postcss.config.js
@@ -1,4 +1,18 @@
 const tailwindcss = require("tailwindcss");
+const cssnano = require("cssnano");
+const purgecss = require("@fullhuman/postcss-purgecss");
+
 module.exports = {
-	plugins: [tailwindcss("./tailwind.config.js"), require("autoprefixer")],
+	plugins: [
+		tailwindcss("./tailwind.config.js"),
+		purgecss({
+			content: ["./src/**/*.html"],
+			defaultExtractor: (content) =>
+				content.match(/[\w-/:]+(?<!:)/g) || [],
+		}),
+		cssnano({
+			preset: "default",
+		}),
+		require("autoprefixer"),
+	],
 };
